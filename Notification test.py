c4d828b4859a5af6a0387b3d02077224e7af6de1
Cori Robinson <cr644@scarletmail.rutgers.edu>
2018-11-01 16:46:53 -0400

    Update Notification test.py

--- Notification test.py
+++ Notification test.py
@@ -1,13 +1,13 @@
+import notify2
 #I need to define a function to send notifications to a user's phone from the app
 #notify2 was installed with pip and imported into Atom
 #Once notify2 was installed, I have to use an init statement to make sure the function
-finds the notifications within the data_url I provide later
+#finds the notifications within the data_url I provide later
 #I set notify equal to the notification from the data_url, the actual content from the data_url
-and the qupte that will appear on the phone when the notification is sent
-#Finally I printed the Nofication 
-def send(content)
-    import notify2
-    notify2.init('data_url')
-    notify = notify2.Notification('data_url', content, 'quote')
+#and the qupte that will appear on the phone when the notification is sent
+#Finally I printed the Nofication
+def send(content):
+    notify2.init('https://www.brainyquote.com/topics/inspirational')
+    notify = notify2.Notification('https://www.brainyquote.com/topics/inspirational', Inspirational, 'Today I chose life.')
     notify.show()
-        print(Notification)
+    print(Notification)
