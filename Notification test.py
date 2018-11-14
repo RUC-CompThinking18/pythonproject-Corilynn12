2a67b92dcfc3d6138dabaff8663cbba55be513d4
Cori Robinson <cr644@scarletmail.rutgers.edu>
2018-11-14 15:49:41 -0500

    Update Notification test.py

--- Notification test.py
+++ Notification test.py
@@ -1,18 +1,16 @@
-#I have to scrape the quotes from a website in order to print them to the terminal. I then have to find a way to send that specific
-#quote as a notification to the terminal. I have to beautifulsoup, requests,and html5lib in order to extract the quotes I need from the website.
-#I also need to import beautiful soup and request that it extracts the data needed from the website
 import requests
 from bs4 import BeautifulSoup
-import csv
-URL = "https://www.goodreads.com/author/quotes/8075577.Rupi_Kaur"
-r = requests.get(URL)
-soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.prettify())
-quotes=[]
-table = soup.find('div', attrs = {'class' : 'quote'})
-file_name = 'Rupi_Kaur_quotes.csv'
-with open(file_name, 'wb') as f:
-    w = csv.DictWriter(f,['url' , 'author'])
-    w.writeheader()
-    for quote in quotes:
-        w.writerow(quote)
+import pynotify
+from time import sleep
+def sendmessage(title, message):
+    pynotify.init("Test")
+    notice = pynotify.Notification('Rupi Kaur' , 'Stay strong!')
+    notice.show()
+    return
+url = "https://quotecatalog.com/quote/rupi-kaur-stay-strong-thr-baVPe57/"
+while True:
+    r = requests.get(url)
+    while r.status_code is not 200:
+            r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html5lib')
+    data = soup.find_all("quotecatalog")
