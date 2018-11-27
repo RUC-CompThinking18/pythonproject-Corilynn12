154f99ade9ce7423343a4e9ccfb75ac7885b2797
Cori Robinson <cr644@scarletmail.rutgers.edu>
2018-11-26 21:23:25 -0500

    Finished python project
    Added the read me file and fixed the function so that it can run properly.

--- python project.py
+++ python project.py
@@ -2,19 +2,19 @@ import schedule
 import time
 #Import schedule and time in order to run the current time and schedule API
 text ='Do not look for healing at the feet of those who broke you.'
+text2 = 'You are the light that most people will never deserve'
 #Wrote out the quote to be printed to the terminal at a certain time
 #function that will hold the text
 def quote():
     print(text)
-#schedule API to run the quote every 10 seconds
+def quote2():
+    print(text2)
+#schedule API to run the quote every  certain amount of seconds
 #Each action I want to the scheduler to do has to be wrote out
 #The quote will print to the terminal every couple seconds or couple minutes
-#I want the quote to print every tuesday and thursday as well
-schedule.every(10).seconds.do(quote)
-schedule.every().day.do(quote)
-schedule.every().tuesday.do(quote)
-schedule.every().thursday.do(quote)
+schedule.every(30).seconds.do(quote)
+schedule.every(01).minutes.do(quote2)
 #As long as the above text is true, the API will run at the assigned days and time
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(60)
